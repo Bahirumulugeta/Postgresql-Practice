d10900503df40c3fc1468ac42965d99c26a961eb
bahiru <bahirumulugeta1@gmail.com>
2022-10-05 13:21:12 +0200

    using limit and offset

--- order-limit.sql
+++ order-limit.sql
@@ -1,1 +1,4 @@
-SELECT * FROM sales ORDER BY date_created;
+-- select only the first x number of rows 
+-- SELECT * FROM sales LIMIT 5; 
+-- select x number of rows after skipping y number of rows
+SELECT * FROM sales LIMIT 3 OFFSET 2; 
