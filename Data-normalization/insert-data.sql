a1fedb7ec3975e60f77e58ca15836de958aa71bd
bahiru <bahirumulugeta1@gmail.com>
2022-10-08 11:50:48 +0200

    inserting data on created tables

---
+++ Data-normalization/insert-data.sql
@@ -0,0 +1,13 @@
+-- INSERT INTO cities (name) VALUES('Addis Ababa'),('Adama'),('Hawasa');
+-- INSERT INTO addresses(street,house_number,city_id) 
+--  VALUES
+-- ('Airport road','5A',1),
+-- ('nazret road','3A',2),
+-- ('gebrel','44',3),
+-- ('haya hulet','1A',1);
+
+INSERT INTO users(first_name,last_name,email,address_id)
+VALUES
+      ('Bahiru','Mulugeta','bahiru@kemercode.com',2),
+      ('Eyob','Henok','eyob@kemercode.com',3),
+      ('Negasi','Kiflom','bahiru@kemercode.com',4);
