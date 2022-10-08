3e7d34391089de5529148109a65e42effe8e115c
bahiru <bahirumulugeta1@gmail.com>
2022-10-08 12:58:05 +0200

    combine multiple join in one query using inner join

---
+++ Data-normalization/combine-multiple-join.sql
@@ -0,0 +1,3 @@
+SELECT u.id,first_name,last_name,email,c.name AS city_name,street,house_number FROM users AS u 
+INNER JOIN addresses AS a ON u.address_id = a.id 
+INNER JOIN cities AS c ON a.city_id = c.id;
