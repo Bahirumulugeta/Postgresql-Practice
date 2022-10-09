2f17b88019fc45552a3e5f9165fcd339a312f7b1
bahiru <bahirumulugeta1@gmail.com>
2022-10-09 20:28:54 +0200

    creating new tables

--- Data-normalization/create-table.sql
+++ Data-normalization/create-table.sql
@@ -1,19 +1,19 @@
 -- CREATE DATABASE users;
-
+CREATE TABLE cities(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(300) NOT NULL
+);
+CREATE TABLE addresses (
+    id SERIAL PRIMARY KEY,
+    street VARCHAR(300) NOT NULL,
+    house_number VARCHAR(50) NOT   NULL,
+    city_id INT NOT NULL  REFERENCES cities (id) ON DELETE CASCADE
+);
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(300) NOT NULL,
     last_name VARCHAR(300) NOT NULL,
     email VARCHAR(300) NOT NULL,
-    address_id INT NOT NULL
+    address_id INT NOT NULL REFERENCES addresses (id) ON DELETE CASCADE
 );
-CREATE TABLE addresses (
-    id SERIAL PRIMARY KEY,
-    street VARCHAR(300) NOT NULL,
-    house_number VARCHAR(50) NOT NULL,
-    city_id INT NOT NULL
-);
-CREATE TABLE cities(
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(300) NOT NULL
-);
+
