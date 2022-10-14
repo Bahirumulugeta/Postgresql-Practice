f216d20ae80ebc0a460a5f91819b85f726a236b8
bahiru <bahirumulugeta1@gmail.com>
2022-10-14 16:15:23 +0200

    like with underscore

--- Built_in-Functions/like-pattern.sql
+++ Built_in-Functions/like-pattern.sql
@@ -1,6 +1,7 @@
 
  SELECT first_name LIKE 'Ba',first_name from employees; --is equal to first_name=='Ba'
  SELECT first_name LIKE 'Ba%',first_name from employees; --is equal to first_name starts with Ba
-  SELECT first_name LIKE '%Ba%',first_name from employees; --is equal to first_name has Ba sub string at any where
+ SELECT first_name LIKE '%Ba%',first_name from employees; --is equal to first_name has Ba sub string at any where
+ SELECT first_name LIKE '_Ba%',first_name from employees; --is equal to first_name of second value is B but what ever for the first value
 
 
