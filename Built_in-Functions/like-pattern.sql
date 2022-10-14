6aa0fec7b0a4fb672c60d9183ac5e42f01355d55
bahiru <bahirumulugeta1@gmail.com>
2022-10-14 16:13:40 +0200

    comparison of sub string

--- Built_in-Functions/like-pattern.sql
+++ Built_in-Functions/like-pattern.sql
@@ -1,5 +1,6 @@
 
  SELECT first_name LIKE 'Ba',first_name from employees; --is equal to first_name=='Ba'
  SELECT first_name LIKE 'Ba%',first_name from employees; --is equal to first_name starts with Ba
- 
+  SELECT first_name LIKE '%Ba%',first_name from employees; --is equal to first_name has Ba sub string at any where
+
 
