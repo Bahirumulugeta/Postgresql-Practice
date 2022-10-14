9587bc79882eaf61fd828c0a3260ff503c1726ea
bahiru <bahirumulugeta1@gmail.com>
2022-10-14 16:12:42 +0200

    usage of like for comparison

--- Built_in-Functions/like-pattern.sql
+++ Built_in-Functions/like-pattern.sql
@@ -1,3 +1,5 @@
 
  SELECT first_name LIKE 'Ba',first_name from employees; --is equal to first_name=='Ba'
+ SELECT first_name LIKE 'Ba%',first_name from employees; --is equal to first_name starts with Ba
  
+
